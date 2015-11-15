0148791384d0a4f87c84ba09ba3bfdbee6196881
ZuLiangWang <329851721@qq.com>
2015-11-15 19:23:52 +0800

    camera

--- YouTuApi/app/src/main/java/com/zuliangwang/youtuapi/interactor/DetectFaceInteractor.java
+++
@@ -1,8 +0,0 @@
-package com.zuliangwang.youtuapi.interactor;
-
-/**
- * Created by zuliangwang on 15/11/5.
- */
-public interface DetectFaceInteractor {
-
-}
--- YouTuApi/app/src/main/java/com/zuliangwang/youtuapi/presenter/BasePresenter.java
+++
@@ -1,9 +0,0 @@
-package com.zuliangwang.youtuapi.presenter;
-
-/**
- * Created by zuliangwang on 15/11/5.
- */
-public interface BasePresenter {
-
-    public void initialized();
-}
